419e3c3482c62004dab38e121ba807790da0154b
BOMMABATHINA NAGA SANDEEP <110225351+Sandeep1629@users.noreply.github.com>
2024-03-08 21:59:50 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,249 +1,241 @@
+// Copyright Agora.io. All rights reserved.
+// This program can return different tokens, according to different request parameters.
+// You can use this program to generate tokens for the following services:
+// - Agora RTC
+// - Agora RTM
+// - Agora FPA
+// - Agora Chat
+// - Agora Education
+// This program is for reference only and is not recommended for direct use in a production environment.
+// If you need to use it in a production environment, you need to add a permission verification mechanism.
+// For example, you can add a parameter to the request body, and then verify the parameter in the program.
+//
+// Run server locally
+// 1. Fill in the AppId and AppCertificate in the code.
+// 2. Run the command "go run server.go".
+// 3. Send a POST request to http://localhost:8080/token/generate.
+//   For example:
+//      curl --location 'http://localhost:8080/token/generate' \
+//      --header 'Content-Type: application/json' \
+//      --data '{
+//          "channelName": "channel_name_test",
+//          "uid": "12345678",
+//          "tokenExpireTs": 3600,
+//          "privilegeExpireTs": 3600,
+//          "serviceRtc": {
+//              "enable": true,
+//              "role": 1
+//          },
+//          "serviceRtm": {
+//              "enable": true
+//          }
+//      }'
+// 4. The response is a token.
+//
+// Run server in docker
+// Run the command "docker run -d -it -p 8080:8080 -e APP_ID=YOUR_APP_ID -e APP_CERTIFICATE=YOUR_APP_CERTIFICATE --name agora-token-service agoracn/token:0.1.2023053011"
+// You can also run the service by modifying the docker-compose.yaml file.
+// Open the docker-compose.yaml file and set APP_ID and APP_CERTIFICATE.
+// Run the command "docker-compose up"
+
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/AgoraIO-Community/go-tokenbuilder/rtctokenbuilder"
-	"github.com/AgoraIO-Community/go-tokenbuilder/rtmtokenbuilder"
+	accessTokenBuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/accesstoken2"
+	rtcTokenBuilder "github.com/AgoraIO/Tools/DynamicKey/AgoraDynamicKey/go/src/rtctokenbuilder2"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
-var appID string
-var appCertificate string
+// Request parameters
+type Request struct {
+	// Unique channel name for the AgoraRTC session in the string format
+	ChannelName string `json:"channelName,omitempty"`
+	// User ID. A 32-bit unsigned integer with a value ranging from 1 to (2^32-1)
+	Uid string `json:"uid,omitempty"`
+	// Represented by the number of seconds elapsed since now. If, for example,
+	// you want to access the Agora Service within 10 minutes after the token is generated,
+	// set tokenExpireTs as 600(seconds).
+	TokenExpireTs uint32 `json:"tokenExpireTs,omitempty"`
+	// Represented by the number of seconds elapsed since now. If, for example,
+	// you want to enable your privilege for 10 minutes, set privilegeExpireTs as 600(seconds).
+	PrivilegeExpireTs uint32                  `json:"privilegeExpireTs,omitempty"`
+	ServiceRtc        RequestServiceRtc       `json:"serviceRtc,omitempty"`
+	ServiceRtm        RequestServiceRtm       `json:"serviceRtm,omitempty"`
+	ServiceFpa        RequestServiceFpa       `json:"serviceFpa,omitempty"`
+	ServiceChat       RequestServiceChat      `json:"serviceChat,omitempty"`
+	ServiceEducation  RequestServiceEducation `json:"serviceEducation,omitempty"`
+}
 
+type RequestServiceRtc struct {
+	Enable bool `json:"enable,omitempty"`
+	// RolePublisher: A broadcaster/host in a live-broadcast profile.
+	// RoleSubscriber: An audience(default) in a live-broadcast profile.
+	Role rtcTokenBuilder.Role `json:"role,omitempty"`
+}
+
+type RequestServiceRtm struct {
+	Enable bool `json:"enable,omitempty"`
+}
+
+type RequestServiceFpa struct {
+	Enable bool `json:"enable,omitempty"`
+}
+
+type RequestServiceChat struct {
+	// Refer to BuildChatUserToken of src/chatTokenBuilder/chatTokenBuilder.go
+	EnableUser bool `json:"enableUser,omitempty"`
+	// Refer to BuildChatAppToken of src/chatTokenBuilder/chatTokenBuilder.go
+	EnableApp bool `json:"enableApp,omitempty"`
+	// The user's id, must be unique.
+	UserUuid string `json:"userUuid,omitempty"`
+}
+
+type RequestServiceEducation struct {
+	// Refer to BuildUserToken of src/educationtokenbuilder/educationtokenbuilder.go
+	EnableUser bool `json:"enableUser,omitempty"`
+	// Refer to BuildAppToken of src/educationtokenbuilder/educationtokenbuilder.go
+	EnableApp bool `json:"enableApp,omitempty"`
+	// The user's id, must be unique.
+	UserUuid string `json:"userUuid,omitempty"`
+}
+
+// Return response
+type Response struct {
+	Token string `json:"token"`
+}
+
+const (
+	// Server port
+	ServerPort = "8080"
+	// Server url
+	Url = "/token/generate"
+
+	// Info code
+	CodeOk        = 0
+	CodeOkMessage = "Success"
+	// Error code
+	CodeErrorParams   = -1
+	CodeErrorGenerate = -2
+)
+
+var (
+	// Notice
+	// Fill the AppId and AppCertificate generated in the Agora Console here.
+	AppId          = "87f323efeda84280a6a4f35ad5e93f80"
+	AppCertificate = "afb25a096bdf427da031b8154b9981a9"
+)
+
+// Init
+// Check appId and appCertificate
 func init() {
-	os.Setenv("APP_ID", "87f323efeda84280a6a4f35ad5e93f80")
-	os.Setenv("APP_CERTIFICATE", "afb25a096bdf427da031b8154b9981a9")
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
+	if appId := os.Getenv("APP_ID"); appId != "" {
+		AppId = appId
+	}
+	if appCertificate := os.Getenv("APP_CERTIFICATE"); appCertificate != "" {
+		AppCertificate = appCertificate
+	}
+
+	if AppId == "" || AppCertificate == "" {
+		log.Printf("check appId or appCertificate")
+		os.Exit(1)
 	}
 }
 
+// Generate token
+func generateToken(request *Request) (token string, err error) {
+	accessToken := accessTokenBuilder.NewAccessToken(AppId, AppCertificate, request.TokenExpireTs)
+
+	// refer to src/rtctokenbuilder2/rtctokenbuilder.go
+	if request.ServiceRtc.Enable {
+		serviceRtc := accessTokenBuilder.NewServiceRtc(request.ChannelName, request.Uid)
+		serviceRtc.AddPrivilege(accessTokenBuilder.PrivilegeJoinChannel, request.PrivilegeExpireTs)
+		if request.ServiceRtc.Role == rtcTokenBuilder.RolePublisher {
+			serviceRtc.AddPrivilege(accessTokenBuilder.PrivilegePublishAudioStream, request.PrivilegeExpireTs)
+			serviceRtc.AddPrivilege(accessTokenBuilder.PrivilegePublishVideoStream, request.PrivilegeExpireTs)
+			serviceRtc.AddPrivilege(accessTokenBuilder.PrivilegePublishDataStream, request.PrivilegeExpireTs)
+		}
+		accessToken.AddService(serviceRtc)
+	}
+
+	// refer to src/rtmtokenbuilder2/rtmtokenbuilder.go
+	if request.ServiceRtm.Enable {
+		serviceRtm := accessTokenBuilder.NewServiceRtm(request.Uid)
+		serviceRtm.AddPrivilege(accessTokenBuilder.PrivilegeLogin, request.TokenExpireTs)
+		accessToken.AddService(serviceRtm)
+	}
+
+	// refer to src/fpatokenbuilder/fpatokenbuilder.go
+	if request.ServiceFpa.Enable {
+		serviceFpa := accessTokenBuilder.NewServiceFpa()
+		serviceFpa.AddPrivilege(accessTokenBuilder.PrivilegeLogin, 0)
+		accessToken.AddService(serviceFpa)
+	}
+
+	// refer to src/chatTokenBuilder/chatTokenBuilder.go
+	if request.ServiceChat.EnableUser {
+		serviceChatUser := accessTokenBuilder.NewServiceChat(request.ServiceChat.UserUuid)
+		serviceChatUser.AddPrivilege(accessTokenBuilder.PrivilegeChatUser, request.TokenExpireTs)
+		accessToken.AddService(serviceChatUser)
+	}
+
+	// refer to src/chatTokenBuilder/chatTokenBuilder.go
+	if request.ServiceChat.EnableApp {
+		serviceChatApp := accessTokenBuilder.NewServiceChat("")
+		serviceChatApp.AddPrivilege(accessTokenBuilder.PrivilegeChatApp, request.TokenExpireTs)
+		accessToken.AddService(serviceChatApp)
+	}
+
+	// refer to src/educationtokenbuilder/educationtokenbuilder.go
+	if request.ServiceEducation.EnableUser {
+		serviceEducationUser := accessTokenBuilder.NewServiceEducation("", request.ServiceEducation.UserUuid, -1)
+		serviceEducationUser.AddPrivilege(accessTokenBuilder.PrivilegeEducationUser, request.TokenExpireTs)
+		accessToken.AddService(serviceEducationUser)
+	}
+
+	// refer to src/educationtokenbuilder/educationtokenbuilder.go
+	if request.ServiceEducation.EnableApp {
+		serviceEducationApp := accessTokenBuilder.NewServiceEducation("", "", -1)
+		serviceEducationApp.AddPrivilege(accessTokenBuilder.PrivilegeEducationApp, request.TokenExpireTs)
+		accessToken.AddService(serviceEducationApp)
+	}
+
+	return accessToken.Build()
+}
+
+// Output response
+func output(c *gin.Context, request *Request, httpStatus int, code int, message string, data interface{}) {
+	log.Printf("request:%+v, httpStatus:%d, code:%d, message:%s", request, httpStatus, code, message)
+	c.JSON(httpStatus, gin.H{"code": code, "message": message, "data": data})
+}
+
+// Start server
 func main() {
-	appIDEnv, appIDExists := os.LookupEnv("APP_ID")
-	appCertEnv, appCertExists := os.LookupEnv("APP_CERTIFICATE")
-
-	if !appIDExists || !appCertExists {
-		log.Fatal("FATAL ERROR: ENV not properly configured, check appID and appCertificate")
-	} else {
-		appID = appIDEnv
-		appCertificate = appCertEnv
-	}
-
-	api := gin.Default()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	api.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	api.Use(nocache())
-	api.GET("rtc/:channelName/:role/:tokentype/:uid/", getRtcToken)
-	api.GET("rtm/:uid/", getRtmToken)
-	api.GET("rte/:channelName/:role/:tokentype/:uid/", getBothTokens)
-	api.Run(":" + port) // listen and serve on localhost:8080
-}
-
-func nocache() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// set headers
-		c.Header("Cache-Control", "private, no-cache, no-store, must-revalidate")
-		c.Header("Expires", "-1")
-		c.Header("Pragma", "no-cache")
-		c.Header("Access-Control-Allow-Origin", "*")
-	}
-}
-
-func getRtcToken(c *gin.Context) {
-	log.Printf("rtc token\n")
-	// get param values
-	channelName, tokentype, uidStr, role, expireTimestamp, err := parseRtcParams(c)
-
-	if err != nil {
-		c.Error(err)
-		c.AbortWithStatusJSON(400, gin.H{
-			"message": "Error Generating RTC token: " + err.Error(),
-			"status":  400,
-		})
-		return
-	}
-
-	rtcToken, tokenErr := generateRtcToken(channelName, uidStr, tokentype, role, expireTimestamp)
-
-	if tokenErr != nil {
-		log.Println(tokenErr) // token failed to generate
-		c.Error(tokenErr)
-		errMsg := "Error Generating RTC token - " + tokenErr.Error()
-		c.AbortWithStatusJSON(400, gin.H{
-			"status": 400,
-			"error":  errMsg,
-		})
-	} else {
-		log.Println("RTC Token generated")
-		c.JSON(200, gin.H{
-			"rtcToken": rtcToken,
-		})
-	}
-}
-
-func getRtmToken(c *gin.Context) {
-	log.Printf("rtm token\n")
-	// get param values
-	uidStr, expireTimestamp, err := parseRtmParams(c)
-
-	if err != nil {
-		c.Error(err)
-		c.AbortWithStatusJSON(400, gin.H{
-			"message": "Error Generating RTC token: " + err.Error(),
-			"status":  400,
-		})
-		return
-	}
-
-	rtmToken, tokenErr := rtmtokenbuilder.BuildToken(appID, appCertificate, uidStr, rtmtokenbuilder.RoleRtmUser, expireTimestamp)
-
-	if tokenErr != nil {
-		log.Println(tokenErr) // token failed to generate
-		c.Error(tokenErr)
-		errMsg := "Error Generating RTM token: " + tokenErr.Error()
-		c.AbortWithStatusJSON(400, gin.H{
-			"error":  errMsg,
-			"status": 400,
-		})
-	} else {
-		log.Println("RTM Token generated")
-		c.JSON(200, gin.H{
-			"rtmToken": rtmToken,
-		})
-	}
-}
-
-func getBothTokens(c *gin.Context) {
-	log.Printf("dual token\n")
-	// get rtc param values
-	channelName, tokentype, uidStr, role, expireTimestamp, rtcParamErr := parseRtcParams(c)
-
-	if rtcParamErr != nil {
-		c.Error(rtcParamErr)
-		c.AbortWithStatusJSON(400, gin.H{
-			"message": "Error Generating RTC token: " + rtcParamErr.Error(),
-			"status":  400,
-		})
-		return
-	}
-	// generate the rtcToken
-	rtcToken, rtcTokenErr := generateRtcToken(channelName, uidStr, tokentype, role, expireTimestamp)
-	// generate rtmToken
-	rtmToken, rtmTokenErr := rtmtokenbuilder.BuildToken(appID, appCertificate, uidStr, rtmtokenbuilder.RoleRtmUser, expireTimestamp)
-
-	if rtcTokenErr != nil {
-		log.Println(rtcTokenErr) // token failed to generate
-		c.Error(rtcTokenErr)
-		errMsg := "Error Generating RTC token - " + rtcTokenErr.Error()
-		c.AbortWithStatusJSON(400, gin.H{
-			"status": 400,
-			"error":  errMsg,
-		})
-	} else if rtmTokenErr != nil {
-		log.Println(rtmTokenErr) // token failed to generate
-		c.Error(rtmTokenErr)
-		errMsg := "Error Generating RTC token - " + rtmTokenErr.Error()
-		c.AbortWithStatusJSON(400, gin.H{
-			"status": 400,
-			"error":  errMsg,
-		})
-	} else {
-		log.Println("RTC Token generated")
-		c.JSON(200, gin.H{
-			"rtcToken": rtcToken,
-			"rtmToken": rtmToken,
-		})
-	}
-
-}
-
-func parseRtcParams(c *gin.Context) (channelName, tokentype, uidStr string, role rtctokenbuilder.Role, expireTimestamp uint32, err error) {
-	// get param values
-	channelName = c.Param("channelName")
-	roleStr := c.Param("role")
-	tokentype = c.Param("tokentype")
-	uidStr = c.Param("uid")
-	expireTime := c.DefaultQuery("expiry", "3600")
-
-	if roleStr == "publisher" {
-		role = rtctokenbuilder.RolePublisher
-	} else {
-		role = rtctokenbuilder.RoleSubscriber
-	}
-
-	expireTime64, parseErr := strconv.ParseUint(expireTime, 10, 64)
-	if parseErr != nil {
-		// if string conversion fails return an error
-		err = fmt.Errorf("failed to parse expireTime: %s, causing error: %s", expireTime, parseErr)
-	}
-
-	// set timestamps
-	expireTimeInSeconds := uint32(expireTime64)
-	currentTimestamp := uint32(time.Now().UTC().Unix())
-	expireTimestamp = currentTimestamp + expireTimeInSeconds
-
-	return channelName, tokentype, uidStr, role, expireTimestamp, err
-}
-
-func parseRtmParams(c *gin.Context) (uidStr string, expireTimestamp uint32, err error) {
-	// get param values
-	uidStr = c.Param("uid")
-	expireTime := c.DefaultQuery("expiry", "3600")
-
-	expireTime64, parseErr := strconv.ParseUint(expireTime, 10, 64)
-	if parseErr != nil {
-		// if string conversion fails return an error
-		err = fmt.Errorf("failed to parse expireTime: %s, causing error: %s", expireTime, parseErr)
-	}
-
-	// set timestamps
-	expireTimeInSeconds := uint32(expireTime64)
-	currentTimestamp := uint32(time.Now().UTC().Unix())
-	expireTimestamp = currentTimestamp + expireTimeInSeconds
-
-	// check if string conversion fails
-	return uidStr, expireTimestamp, err
-}
-func generateRtcToken(channelName, uidStr, tokentype string, role rtctokenbuilder.Role, expireTimestamp uint32) (rtcToken string, err error) {
-	log.Printf(appID, appCertificate)
-	if tokentype == "userAccount" {
-		log.Printf("Building Token with userAccount: %s\n", uidStr)
-		rtcToken, err = rtctokenbuilder.BuildTokenWithUserAccount(appID, appCertificate, channelName, uidStr, role, expireTimestamp)
-		return rtcToken, err
-
-	} else if tokentype == "uid" {
-		uid64, parseErr := strconv.ParseUint(uidStr, 10, 64)
-		// check if conversion fails
-		if parseErr != nil {
-			err = fmt.Errorf("failed to parse uidStr: %s, to uint causing error: %s", uidStr, parseErr)
-			return "", err
+	r := gin.Default()
+	r.POST(Url, func(c *gin.Context) {
+		// Get params
+		var request Request
+		if err := c.BindJSON(&request); err != nil {
+			output(c, &request, http.StatusBadRequest, CodeErrorParams, err.Error(), "")
+			return
 		}
 
-		uid := uint32(uid64) // convert uid from uint64 to uint 32
-		log.Printf("Building Token with uid: %d\n", uid)
-		rtcToken, err = rtctokenbuilder.BuildTokenWithUID(appID, appCertificate, channelName, uid, role, expireTimestamp)
-		return rtcToken, err
+		// Generate token
+		token, err := generateToken(&request)
+		if err != nil {
+			output(c, &request, http.StatusBadRequest, CodeErrorGenerate, err.Error(), "")
+			return
+		}
 
-	} else {
-		err = fmt.Errorf("failed to generate RTC token for Unknown Tokentype: %s", tokentype)
-		log.Println(err)
-		return "", err
-	}
+		// Return response
+		response := &Response{Token: token}
+		output(c, &request, http.StatusOK, CodeOk, CodeOkMessage, response)
+	})
+
+	log.Printf("start server, appId:%s, serverPort:%s, url:%s", AppId, ServerPort, Url)
+	// Start server
+	r.Run(ServerPort)
 }
